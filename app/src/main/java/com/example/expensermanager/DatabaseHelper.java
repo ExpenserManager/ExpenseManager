ea4d744f73b26d91005675fe9e1b41c73555fe63
lisat <lisatsch@edu.aau.at>
2024-06-14 17:19:34 +0200

    add DatabaseHelper class

---
+++ app/src/main/java/com/example/expensermanager/DatabaseHelper.java
@@ -0,0 +1,35 @@
+package com.example.expensermanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+   private static final String DB_NAME = "expenser_manager.db";
+   private static final int DB_VERSION = 1;
+   private static final String TABLE_NAME = "expense_manager";
+
+   //Spalten
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_CATEGORY = "category";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_AMOUNT = "amount";
+
+
+
+
+    public DatabaseHelper(Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
