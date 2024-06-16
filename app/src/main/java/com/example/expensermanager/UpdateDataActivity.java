c79c5126c9c276251eeaf0956ffcc8d0799c6763
lisat <lisatsch@edu.aau.at>
2024-06-16 13:28:44 +0200

    add intent to transfer data back to ExpenseViewActivity.java

--- app/src/main/java/com/example/expensermanager/UpdateDataActivity.java
+++ app/src/main/java/com/example/expensermanager/UpdateDataActivity.java
@@ -2,6 +2,8 @@ package com.example.expensermanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +17,10 @@ import com.example.expensermanager.databinding.ActivityMainBinding;
 public class UpdateDataActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private DatabaseHelper dbHelper;
+    String description;
+    String amount;
+    String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,15 +34,40 @@ public class UpdateDataActivity extends AppCompatActivity {
         });
         EditText descriptionField = findViewById(R.id.description);
         EditText amountField = findViewById(R.id.amount);
+        Button updateButton = findViewById(R.id.update_button);
 
 
         Intent intent = getIntent();
         if(intent != null){
-           String description = intent.getStringExtra("description");
-           String amount = intent.getStringExtra("amount");
+                description = intent.getStringExtra("description");
+                amount = intent.getStringExtra("amount");
+                id = intent.getStringExtra("id");
 
            descriptionField.setText(description);
            amountField.setText(amount);
         }
+
+
+        dbHelper = new DatabaseHelper(UpdateDataActivity.this);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newDescription = descriptionField.getText().toString();
+                String newAmount = amountField.getText().toString();
+                dbHelper.updateData(id, newDescription, newAmount); //update database
+
+              //give data back to ExpenseViewActivity
+                Intent goBackIntent = new Intent();
+                goBackIntent.putExtra("id", id);
+                goBackIntent.putExtra("description", newDescription);
+                goBackIntent.putExtra("amount", newAmount);
+                setResult(RESULT_OK, goBackIntent);
+                finish();
+
+            }
+        });
+
+
+
     }
 }
