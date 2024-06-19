7180ada3dceb45fbad13ad2fa149cca35f81083f
lisat <lisatsch@edu.aau.at>
2024-06-19 14:15:58 +0200

    add insert method for second table

--- app/src/main/java/com/example/expensermanager/DatabaseHelper.java
+++ app/src/main/java/com/example/expensermanager/DatabaseHelper.java
@@ -63,7 +63,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
 
-    public void insertData(DatabaseHelper dbHelper, String category, String description, double amount, String date){
+    public void insertData(DatabaseHelper dbHelper, String category, String description, double amount, String date, String tableName){
 
         //data repository gets in write mode
         SQLiteDatabase db = dbHelper.getWritableDatabase();
@@ -77,7 +77,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
         //null if the ContentnValues map is empty - no insertion
-        long newRowId = db.insert(TABLE_NAME, null, values);
+        long newRowId = db.insert(tableName, null, values);
 
         //db.insert() returns -1 if the insertion has failed
         if(newRowId == -1){
@@ -88,27 +88,45 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
-    public void deleteData(DatabaseHelper dbHelper, String id) {
+    public void insertCategory(DatabaseHelper dbHelper, String categoryName, String color, String tableName){
+
+        //data repository gets in write mode
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        String query = "DELETE FROM " + TABLE_NAME + " WHERE _id=" + id; // SQL Query - deleting row via id
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_CATEGORY_CATEGORY_TABLE, categoryName);
+        values.put(COLUMN_COLOR_CATEGORY_TABLE, color);
+
+        long newRowId = db.insert(tableName, null, values);
+        if(newRowId == -1){
+            Toast.makeText(context, "insertion failed", Toast.LENGTH_SHORT);
+        }else{
+            Toast.makeText(context, "Successfully Added!", Toast.LENGTH_SHORT);
+        }
+    }
+
+
+    public void deleteData(DatabaseHelper dbHelper, String id, String tableName) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String query = "DELETE FROM " + tableName + " WHERE _id=" + id; // SQL Query - deleting row via id
         db.execSQL(query);
 
     }
 
-    public void updateData(String id, String description, String amount, String date){
+    public void updateData(String id, String description, String amount, String date, String tableName){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
 
         values.put(COLUMN_DESCRIPTION, description);
         values.put(COLUMN_AMOUNT, amount);
         values.put(COLUMN_DATE, date);
-        db.update(TABLE_NAME, values, "_id=?", new String[]{id});
+        db.update(tableName, values, "_id=?", new String[]{id});
 
     }
 
 
-    Cursor readAllData(){
-        String query = "SELECT * FROM " + TABLE_NAME;
+    Cursor readAllData(String tableName){
+        String query = "SELECT * FROM " + tableName;
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = null;
