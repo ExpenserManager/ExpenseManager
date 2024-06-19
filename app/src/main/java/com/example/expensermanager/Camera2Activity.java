63e1887702e2b3052a270d8eb2b9eff93beef525
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-06-19 11:15:06 +0200

    add files of SonarLint

---
+++ app/src/main/java/com/example/expensermanager/Camera2Activity.java
@@ -0,0 +1,39 @@
+package com.example.expensermanager;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+public class Camera2Activity extends AppCompatActivity {
+    FrameLayout frameLayout;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camera2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.camera2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        loadFragment(new CameraFragment(), false);
+
+    }
+    public void loadFragment(Fragment fragment, Boolean bool) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        if (bool)
+            transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/expensermanager/CameraActivity.java
@@ -0,0 +1,58 @@
+package com.example.expensermanager;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CameraActivity extends AppCompatActivity {
+    // Define the pic id
+    private static final int pic_id = 123;
+    // Define the button and imageview type variable
+    Button camera_open_id;
+    ImageView click_image_id;     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camera);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.camera), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        camera_open_id= findViewById(R.id.camera_button);
+        // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
+        camera_open_id = findViewById(R.id.camera_button);
+        click_image_id = findViewById(R.id.click_image);
+
+        // Camera_open button is for open the camera and add the setOnClickListener in this button
+        camera_open_id.setOnClickListener(v -> {
+            // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // Start the activity with camera_intent, and request pic id
+            startActivityForResult(camera_intent, pic_id);
+        });
+    }
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Match the request 'pic id with requestCode
+        if (requestCode == pic_id) {
+            // BitMap is data structure of image file which store the image in memory
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // Set the image in imageview for display
+            click_image_id.setImageBitmap(photo);
+        }
+    }
+
+}
