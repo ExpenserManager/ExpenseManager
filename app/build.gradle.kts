05f5fae15f9991b5c4da85cc0342f9e4bc839ef0
marrabl <162188192+marrabl@users.noreply.github.com>
2024-07-16 00:10:25 +0200

    add blur effect to StartActivity when login is selected

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern.compile
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -43,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(libs.blurry)
 }
--- app/src/main/java/com/example/expensermanager/StartActivity.java
+++ app/src/main/java/com/example/expensermanager/StartActivity.java
@@ -1,8 +1,10 @@
 package com.example.expensermanager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,8 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.expensermanager.databinding.ActivityStartBinding;
 
+import jp.wasabeef.blurry.Blurry;
+
 public class StartActivity extends AppCompatActivity {
     private ActivityStartBinding binding;
 
@@ -21,12 +25,10 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityStartBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        setContentView(binding.getRoot());
 
         EdgeToEdge.enable(this);
-
-        ViewCompat.setOnApplyWindowInsetsListener(view, (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -34,6 +36,15 @@ public class StartActivity extends AppCompatActivity {
 
         binding.loginButton.setOnClickListener(v -> {
             if (savedInstanceState == null) {
+
+                Blurry.with(this).radius(15)
+                        .sampling(2)
+                        .onto(binding.start);
+
+                binding.loginButton.setVisibility(View.INVISIBLE);
+                binding.titleTextView.setVisibility(View.INVISIBLE);
+                binding.signupButton.setVisibility(View.INVISIBLE);
+
                 LoginFragment loginFragment = new LoginFragment();
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, loginFragment)
