013befd1e94b6ebc71ce076c9a69f2b312bf9f84
marrabl <162188192+marrabl@users.noreply.github.com>
2024-07-16 13:27:31 +0200

    add SignUpActivity and binding

--- app/src/main/java/com/example/expensermanager/SignUpActivity.java
+++ app/src/main/java/com/example/expensermanager/SignUpActivity.java
@@ -1,4 +1,20 @@
 package com.example.expensermanager;
 
-public class SignUpActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.expensermanager.databinding.ActivitySignupBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignupBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 }
