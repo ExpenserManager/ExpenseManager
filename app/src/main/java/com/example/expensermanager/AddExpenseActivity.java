742f19e6cf31ec33be50458aed79f7c8e4b792b7
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-07-22 15:02:00 +0200

    add methods to take picture and safe it
    I have implemented dispatchTakePictureIntent() which first creates a new File where the picture will be stored with the help of createImageFile(). Here the File is created with a specific name and location.
    After that th camera is opened by an Intent.
    Furthermore, I added the onActivityResult() to add the taken picture into the corresponding ImageView.

--- app/src/main/java/com/example/expensermanager/AddExpenseActivity.java
+++ app/src/main/java/com/example/expensermanager/AddExpenseActivity.java
@@ -3,17 +3,24 @@ package com.example.expensermanager;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -21,13 +28,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.expensermanager.databinding.ActivityAddExpenseBinding;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddExpenseActivity extends AppCompatActivity {
 
     ActivityAddExpenseBinding binding;
     private DatabaseHelper dbHelper;
+    private ImageView expenseImageView;
+
+    Uri photoURI;
     final String[] category = new String[1];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,7 +100,7 @@ public class AddExpenseActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-               category[0] = spinnerList.get(position);
+                category[0] = spinnerList.get(position);
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -93,26 +108,90 @@ public class AddExpenseActivity extends AppCompatActivity {
             }
         });
 
+        expenseImageView = findViewById(R.id.expense_image_view);
 
+        binding.receiptButton.setOnClickListener(v -> dispatchTakePictureIntent());
 
+    }
+    private String currentPhotoPath;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (photoFile != null) {
+                // Uri -> used to identify resources (such as web pages or
+                // other files, but also e-mail recipients, for example) on the Internet.
+                // this -> activity
+                // com.example.android.fileprovider  must be declared in manifest
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.expensermanager.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_PIC_REQUEST); // starts the camera activity
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        Log.d("Path", ""+currentPhotoPath);
+        return image;
+    }
+    private static final int CAMERA_PIC_REQUEST = 2500;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == CAMERA_PIC_REQUEST && resultCode == RESULT_OK) {
+            // Lade das Bild aus dem Pfad in die ImageView
+            File imgFile = new File(currentPhotoPath);
+            if (imgFile.exists()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(currentPhotoPath);
+                Log.d("Path", "2 "+currentPhotoPath);
+
+                expenseImageView.setImageBitmap(myBitmap);
+
+            }
+        }
     }
 
     private void insertFromInput(){
         String description = binding.description.getText().toString();
         String value = binding.amount.getText().toString();
         String date = binding.date.getText().toString();
+//        String imagePath = binding.expenseImageView.toString();
+        String imagePath = currentPhotoPath;
+        Log.d("Path", "3 "+currentPhotoPath);
+
         Toast.makeText(this, description, Toast.LENGTH_SHORT).show();
 
         Log.d("InsertFromInput", "Description: " + description);
         Log.d("InsertFromInput", "Amount: " + value);
         Log.d("InsertFromInput", "Date: " + date);
         Log.d("InsertFromInput", "Category: " + category[0]);
+        Log.d("InsertFromInput", "ImagePath: " + imagePath);
 
-        dbHelper.insertData(dbHelper, category[0], description, Double.parseDouble(value) ,date, "expense_manager");
+
+        dbHelper.insertData(dbHelper, category[0], description, Double.parseDouble(value) ,date, "expense_manager",imagePath);
         Intent intent = new Intent(AddExpenseActivity.this, ExpenseViewActivity.class);
+        //intent.putExtra("Image_path", currentPhotoPath);
         startActivity(intent);
         finish();
 
