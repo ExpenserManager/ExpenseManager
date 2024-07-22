074a055f87d429a44b7861416982627904eeaeee
lisat <lisatsch@edu.aau.at>
2024-07-22 11:13:15 +0200

    update design of ExpenseViewActivity.java

--- app/src/main/java/com/example/expensermanager/CustomAdapter.java
+++ app/src/main/java/com/example/expensermanager/CustomAdapter.java
@@ -71,7 +71,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, UpdateDataActivity.class);
-                //intent.putExtra("id", String.valueOf(id.get(position)));
+                intent.putExtra("id", String.valueOf(id.get(position)));
                 intent.putExtra("description", String.valueOf(description.get(position)));
                 intent.putExtra("amount", String.valueOf(amount.get(position)));
                 intent.putExtra("date", String.valueOf(date.get(position)));
@@ -115,7 +115,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
        ImageButton deleteButton; //accessing the deleteButton was done with help of chatGPT
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            //id_holder = itemView.findViewById(R.id.ID);
+            id_holder = itemView.findViewById(R.id.ID);
             description_holder = itemView.findViewById(R.id.description);
             amount_holder = itemView.findViewById(R.id.amount);
             date_holder = itemView.findViewById(R.id.date_textView);
