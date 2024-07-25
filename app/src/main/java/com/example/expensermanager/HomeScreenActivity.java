bc26103ebf42e717c6a4f7f8694c509aa9ab8196
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-07-25 13:08:10 +0200

    add qrButton to the HomeScreenActivity
    I have added the new qrButton to the HomeScreenActivity with the feature that the button will bounce slightly when it is clicked. Furthermore, i have initialized two transition xmls for the bounce effect. The onTouchListener() must return false so that the onClickListener() is able to perform its task. The user is then navigated to the QR_Reader class.

--- app/src/main/java/com/example/expensermanager/HomeScreenActivity.java
+++ app/src/main/java/com/example/expensermanager/HomeScreenActivity.java
@@ -1,19 +1,25 @@
 package com.example.expensermanager;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.expensermanager.databinding.ActivityHomeScreenBinding;
-
 
 public class HomeScreenActivity extends AppCompatActivity {
 
-
     private ActivityHomeScreenBinding binding;
+    private Animation scaleAnimation;
+    private Animation bounceAnimation;
 
+    @SuppressLint({"ResourceType", "ClickableViewAccessibility"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,9 @@ public class HomeScreenActivity extends AppCompatActivity {
                     .add(binding.fragmentContainerViewBarchart.getId(), BarchartFragment.class, null)
                     .commit();
         }
+        // values in the xmls from ChatGPT
+        scaleAnimation = AnimationUtils.loadAnimation(this, R.transition.scale);
+        bounceAnimation = AnimationUtils.loadAnimation(this, R.transition.bounce);
 
         binding.addButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +44,31 @@ public class HomeScreenActivity extends AppCompatActivity {
             }
         });
 
+        binding.qrButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.startAnimation(scaleAnimation);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        v.startAnimation(bounceAnimation);
+                        break;
+                }
+                return false;
+            }
+        });
 
+        binding.qrButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeScreenActivity.this, "QR Button Clicked", Toast.LENGTH_SHORT).show();
+                Log.d("HomeScreenActivity", "QR Button Clicked");
+                Intent intent = new Intent(HomeScreenActivity.this, QR_Reader.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
