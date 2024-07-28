b2859587a09245d3ae40113ee1326407dbbbfbf1
Katharina Krassnitzer <k1krassnit@edu.aau.at>
2024-07-28 18:59:03 +0200

    update DayAdapter.java Updated Methods, added binding, removed old imports

--- app/src/main/java/com/example/expensermanager/DayAdapter.java
+++ app/src/main/java/com/example/expensermanager/DayAdapter.java
@@ -4,16 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
+import com.example.expensermanager.databinding.DayItemBinding;
 import java.util.Set;
 
 public class DayAdapter extends RecyclerView.Adapter<DayAdapter.DayViewHolder> {
-
     private final Context context;
     private final Set<Integer> eventDays;
     private final int[] days;
@@ -27,20 +23,22 @@ public class DayAdapter extends RecyclerView.Adapter<DayAdapter.DayViewHolder> {
     @NonNull
     @Override
     public DayViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.day_item, parent, false);
-        return new DayViewHolder(view);
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        DayItemBinding dayItemBinding = DayItemBinding.inflate(layoutInflater, parent, false);
+
+        return new DayViewHolder(dayItemBinding);
     }
 
+    //Method from ChatGBT
     @Override
     public void onBindViewHolder(@NonNull DayViewHolder holder, int position) {
         int day = days[position];
-        holder.dayTextView.setText(String.valueOf(day));
+        holder.dayItemBinding.dayTextView.setText(String.valueOf(day));
 
-        // Check if there is an event on this day
         if (eventDays.contains(day)) {
-            holder.eventMarker.setVisibility(View.VISIBLE);
+            holder.dayItemBinding.eventMarker.setVisibility(View.VISIBLE);
         } else {
-            holder.eventMarker.setVisibility(View.GONE);
+            holder.dayItemBinding.eventMarker.setVisibility(View.GONE);
         }
     }
 
@@ -50,13 +48,12 @@ public class DayAdapter extends RecyclerView.Adapter<DayAdapter.DayViewHolder> {
     }
 
     static class DayViewHolder extends RecyclerView.ViewHolder {
-        TextView dayTextView;
-        ImageView eventMarker;
+        private DayItemBinding dayItemBinding;
 
-        public DayViewHolder(@NonNull View itemView) {
-            super(itemView);
-            dayTextView = itemView.findViewById(R.id.dayTextView);
-            eventMarker = itemView.findViewById(R.id.eventMarker);
+        //Help from ChatGBT for binding
+        public DayViewHolder(@NonNull DayItemBinding dayItemBinding) {
+            super(dayItemBinding.getRoot());
+            this.dayItemBinding = dayItemBinding;
         }
     }
 }
